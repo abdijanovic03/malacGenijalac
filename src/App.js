74616dc8e74d5bff9d9aa18387ba2a900a34d946
Mirza Abdijanovic <mirzaabdijanovic9@gmail.com>
2023-06-08 17:25:45 +0200

    header backround and maincontent start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logoSpacers.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="screen">
+      <img src={logo} className="logoSpacers" alt="Spacers" />
+      <div className="mainContent">
+        <p className="aboutGameText">Inovativna igra koja spaja AI, AR i klasičnu arkadnu akciju</p>
+        <p className="aboutGameHeader">SPACERS</p>
+        <p className="aboutGameSubheader">3 KARAKTERA</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
